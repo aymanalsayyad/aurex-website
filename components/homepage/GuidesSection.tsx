f7e6d4f538e180ff3440d37d0b6a2cb80476d4af
ayman <aymanalsayyad@gmail.com>
2025-11-11 16:24:51 +0400

    Remove unused FileText import

--- components/homepage/GuidesSection.tsx
+++ components/homepage/GuidesSection.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Download, FileText } from 'lucide-react';
+import { Download } from 'lucide-react';
 import Link from 'next/link';
 
 export default function GuidesSection() {
